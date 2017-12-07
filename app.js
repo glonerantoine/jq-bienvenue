c0874585159d7078518c9b0e5d2b518c617332e5
Gloner <antoine.gloner@orange.frgit config --global user.name Antoinegit user.email antoine.gloner@orange.fr>
2017-12-07 16:50:55 +0100

    ajout d'une fonction sur le fichier JS et ajout d'un bouton pour la fonction sur le fichier HTML

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('have fun !');
+
+$("button").click(function(){
+
+var info = {
+	first_name : $(first_name).val(),
+	last_name : $(last_name).val(),
+	city : $(city).val()
+}
+console.log("Hi")
+console.log(info)
+console.log(info.first_name,info.last_name,info.city);
+
+$("#username").html(info.first_name+" "+info.last_name+" "+info.city)
+
+
+
+
+});
+
+
+/* réussir à insérer dans la console */
